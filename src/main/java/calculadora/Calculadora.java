ac8052be7f91be3aee2892d1593a3356e2e7a604
ConnyMoreno <connymoreno25@gmail.com>
2024-12-05 11:31:16 -0500

    modificar

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -4,10 +4,36 @@
  */
 package calculadora;
 
+import java.util.Stack;
+
 /**
  *
  * @author ASOCIACIÒN
  */
 public class Calculadora {
+    private double dato1;
+    private double dato2;
+    private Stack<String> Deshacer;
+    private Stack<String> Rehacer;
+    private Stack<String> resultado;
+    
+
+    public Calculadora(double dato1, double dato2, Stack<String> Deshacer, Stack<String> Rehacer,Stack<String> resultado) {
+        this.dato1 = dato1;
+        this.dato2 = dato2;
+        Deshacer = new Stack<>();
+        Rehacer = new Stack<>();
+        resultado = new Stack<>();
+        
+    }
+
+  
+    
+         public void sumar(String calculadora){
+             Deshacer.pop();
+             
+        
+    }
+    
     
 }
--- src/main/java/com/mycompany/nayelimoreno2025/Nayelimoreno2025.java
+++ src/main/java/com/mycompany/nayelimoreno2025/Nayelimoreno2025.java
@@ -11,6 +11,6 @@ package com.mycompany.nayelimoreno2025;
 public class Nayelimoreno2025 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
