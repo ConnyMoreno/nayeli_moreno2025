59fde0e2e1602967662cd09930175f03f91872bc
ConnyMoreno <connymoreno25@gmail.com>
2024-12-05 11:21:32 -0500

    first commit

---
+++ src/main/java/com/mycompany/nayelimoreno2025/Nayelimoreno2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nayelimoreno2025;
+
+/**
+ *
+ * @author ASOCIACIÒN
+ */
+public class Nayelimoreno2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
